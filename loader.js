3cd663a7a28f352ef57018d80e317e7d708090b8
RainerAtSpirit <R.Wittmann@spirit.de>
2014-01-18 17:10:28 +0100

    add password bearer token handling

--- loader.js
+++ loader.js
@@ -1,37 +1,48 @@
-/*global $, console*/
+/* globals $data, $, console, runOnce, prompt*/
+
 (function() {
     'use strict';
+    var runOnce = window.runOnce = typeof window.runOnce === 'undefined',
+        token;
 
-    var username = '',
-        password = '',
-        hostname = '',
-        runOnce = window.runOnce = true,
-        token,
-        scripts = [
-            'lib/datajs/datajs-1.1.1.min.js',
-            'lib/jayData/1.3.5/jaydata.min.js',
-            'lib/jayData/1.3.5/jaydataproviders/oDataProvider.min.js',
-            'lib/jayData/1.3.5/jaydatamodules/deferred.min.js'
-        ],
-        index = 0;
+    init();
 
-
-    if (runOnce){
-        init();
+    function runTestCode () {
+        console.log('ctx', window.ctx);
     }
 
 
 
+    // 1. load infrastructure files from remote host on first time
+    // 2. get token to access secure endpoints
+    // 3. setup ctx to add bearer token
 
+    // When all done call runTestCode()
     function init () {
-        loadScript();
+        //store ctx and token globally
+        var libsUrl = 'http://rainerat.spirit.de/testLibs/',
+            scripts = [
+                'lib/datajs/datajs-1.1.1.min.js',
+                'lib/jayData/1.3.5/jaydata.min.js',
+                'lib/jayData/1.3.5/jaydataproviders/oDataProvider.min.js',
+                'lib/jayData/1.3.5/jaydatamodules/deferred.min.js'
+            ],
+            index = 0;
 
-        function loadScript(){
+        console.log('init', runOnce);
+        if ( runOnce ) {
+            loadScript();
+        }
+        else {
+            runTestCode();
+        }
+
+        function loadScript () {
             //make sure the current index is still a part of the array
             if ( index < scripts.length ) {
 
                 //get the script at the current index
-                $.getScript(scripts[index], function() {
+                $.getScript(libsUrl + scripts[index], function() {
 
                     //once the script is loaded, increase the index and attempt to load the next script
                     console.log('Loaded: ' + scripts[index]);
@@ -39,21 +50,59 @@
                     loadScript();
                 });
             }
-            else{
+            else {
+                runOnce = false;
                 getToken();
             }
         }
+
+        function getToken () {
+            var username = prompt('username'),
+                password = prompt('password');
+
+            $.ajax('/Token', {
+                type: 'POST',
+                data: 'grant_type=password&username=' + username + '&password=' + password
+            }).then(function( resp ) {
+                    token = resp.access_token;
+
+                }).then(function() {
+                    $data.service('/odata/$metadata', function( contextFactory, contextType ) {
+                        var entityMap = [];
+
+                        var context = contextFactory();
+                        context.onReady(function() {
+                            window.ctx = context;
+
+                            context.prepareRequest = function( cfg ) {
+                                cfg[0].headers.Authorization = 'bearer ' + token;
+                            };
+
+                            context.forEachEntitySet(function( entity ) {
+                                entityMap.push({
+                                    hash: '#svcId/' + encodeURIComponent(entity.name),
+                                    name: entity.name
+                                });
+                            });
+
+                        }).then(function() {
+                                runTestCode();
+                            });
+
+                    }, { httpHeaders: { 'Authorization': 'bearer ' + token }});
+                });
+        }
     }
-
-    function getToken () {
-
-        //Getting a token and using it in plupload requests
-        $.ajax('/Token', {
-            type: 'POST',
-            data: 'grant_type=password&username=' + username + '&password=' + password
-        }).then(function( resp ) {
-                token = resp.access_token;
-            });
-    }
-
 })();
+
+
+
+
+
+
+
+
+
+
+
+
